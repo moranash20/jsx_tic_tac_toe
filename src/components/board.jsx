ced0fb08b43d7c022b75486f0ddd67a3bb8c185a
moranash20 <moranash20@gmail.com>
2023-07-27 10:48:46 +0300

    commit message

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,4 +1,6 @@
 import { useState} from "react";
+import Confetti from 'react-confetti';
+import ringer from '../winGame.mp3';
 import Cell from "./cell";
 import Win from "./win"
 
@@ -12,35 +14,8 @@ export default function Board() {
     const [isDraw, setIsDraw] = useState(false);
     const [countX, setCountX] = useState(0);
     const [countO, setCountO] = useState(0);
-    const [showFireworks, setShowFireworks] = useState(false);
 
-    let winCombo1 = cells[0][0] && cells[0][1] && cells[0][2]
-    let winCombo2 = cells[1][0] && cells[1][1] && cells[1][2]
-    let winCombo3 = cells[2][0] && cells[2][1] && cells[2][2]
-    let winCombo4 = cells[0][0] && cells[1][0] && cells[2][0]
-    let winCombo5 = cells[0][1] && cells[1][1] && cells[2][1]
-    let winCombo6 = cells[0][2] && cells[1][2] && cells[2][2]
-    let winCombo7 = cells[0][0] && cells[1][1] && cells[2][2]
-    let winCombo8 = cells[0][2] && cells[1][1] && cells[2][0]
-
-    let winCombos = {
-        'firstRow': winCombo1,
-        'secondRow': winCombo2,
-        'thirdRow': winCombo3,
-        'firstColumn': winCombo4,
-        'secondColumn': winCombo5,
-        'thirdColumn': winCombo6,
-        'diagonal1': winCombo7,
-        'diagonal2': winCombo8
-        // { winCombo1: cells[0][0] && cells[0][1] && cells[0][2] },
-        // { winCombo2: cells[1][0] && cells[1][1] && cells[1][2] },
-        // { winCombo3: cells[2][0] && cells[2][1] && cells[2][2] },
-        // { winCombo4: cells[0][0] && cells[1][0] && cells[2][0] },
-        // { winCombo5: cells[0][1] && cells[1][1] && cells[2][1] },
-        // { winCombo6: cells[0][2] && cells[1][2] && cells[2][2] },
-        // { winCombo7: cells[0][0] && cells[1][1] && cells[2][2] },
-        // { winCombo8: cells[0][2] && cells[1][1] && cells[2][0] },
-    }
+    const audio = new Audio(ringer);
 
     const checkWinner = (newCells) => {
         // Check rows for a winner
@@ -52,8 +27,6 @@ export default function Board() {
             ) {
                 setIsWin(true);
                 return newCells[row][0]
-                // setWinner(player => player);
-                // return;
             }
         }
 
@@ -64,7 +37,6 @@ export default function Board() {
                 newCells[0][col] === newCells[1][col] &&
                 newCells[1][col] === newCells[2][col]
             ) {
-                // setWinner(newCells[0][col]);
                 setIsWin(true);
                 return newCells[0][col];
             }
@@ -76,7 +48,6 @@ export default function Board() {
             newCells[0][0] === newCells[1][1] &&
             newCells[1][1] === newCells[2][2]
         ) {
-            // setWinner(newCells[0][0]);
             setIsWin(true);
             return newCells[0][0];
         }
@@ -86,10 +57,10 @@ export default function Board() {
             newCells[0][2] === newCells[1][1] &&
             newCells[1][1] === newCells[2][0]
         ) {
-            // setWinner(newCells[0][2]);
             setIsWin(true);
             return newCells[0][2];
         }
+
         // If no winner is found, check for a draw
         checkDraw(newCells);
     }
@@ -116,10 +87,13 @@ export default function Board() {
 
             let win = checkWinner(cells);
             setWinner(win);
+            
             if (win === 'X') {
+                audio.play();
                 setCountX(countX + 1);
             }
             else if(win === 'O'){
+                audio.play();
                 setCountO(countO + 1);
             }
 
@@ -141,7 +115,7 @@ export default function Board() {
             <div className="title">
                 <b>Tic Tac Toe</b>
             </div>
-            <div className="player">Next Player: {currentPlayer}</div>
+            <h2 className="player">Next Player: {currentPlayer}</h2>
             <div className="board">
                 {
                     cells.map((row, rowIndex) => {
@@ -154,11 +128,11 @@ export default function Board() {
                 <div className="points">
                     <table >
                         <th id="table_title"> Points </th>
-                        <tr>
+                        <tr className="table-head">
                             <th> X </th>
                             <th> O </th>
                         </tr>
-                        <tr>
+                        <tr className="table-body">
                             <td>{countX}</td>
                             <td>{countO}</td>
                         </tr>
@@ -166,11 +140,17 @@ export default function Board() {
                 </div>
             </div>
             <div className="win">            
-                <b> {winner } {winner && isWin && 'win '} </b>
+                <b> {winner } { winner && isWin  &&'win '}
+                 {isWin && <Confetti/>}
+                 {/* {isWin && playSound} */}
+                </b>
                 <b className="draw">{isDraw && 'Draw'}</b>
             </div>
             <p></p>
             <button className="reset" onClick={handleReset}>Reset Board</button>
+            
+            
+            
         </>
     );
 }
